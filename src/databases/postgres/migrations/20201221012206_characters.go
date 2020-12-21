bccbda2149ebe6ab995116e2f90906bb4033775e
Víctor Hugo Valle Castillo <victouk@gmail.com>
2020-12-20 22:51:00 -0300

    Adding migration settings and some samples.

---
+++ src/databases/postgres/migrations/20201221012206_characters.go
@@ -0,0 +1,16 @@
+
+package main
+
+import (
+	"github.com/jinzhu/gorm"
+)
+
+// Up is executed when this migration is applied
+func Up_20201221012206(txn *gorm.DB) {
+
+}
+
+// Down is executed when this migration is rolled back
+func Down_20201221012206(txn *gorm.DB) {
+
+}
