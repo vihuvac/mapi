40b64102285550e28d74813b07c4999a9c4cc3dd
Víctor Hugo Valle Castillo <victouk@gmail.com>
2021-08-27 20:32:14 -0300

    Updating migration script for the updated character schema.

--- src/databases/postgres/migrations/20201221012206_characters.go
+++ src/databases/postgres/migrations/20201221012206_characters.go
@@ -1,15 +1,40 @@
 package migrations
 
 import (
+	"fmt"
+	"log"
+
 	"github.com/jinzhu/gorm"
 )
 
 // Up is executed when this migration is applied
-func Up_20201221012206(txn *gorm.DB) {
+func Up_20201221012206(db *gorm.DB) {
+	msg := "Characters migration 20201221012206 up!"
+	fmt.Println(msg)
 
+	err := db.Exec(`
+    ALTER TABLE characters ADD CONSTRAINT unique_nickname UNIQUE (nickname);
+	`).Error
+
+	if err != nil {
+		log.Println("Migration up error:", err)
+	}
+
+	fmt.Println(msg + "was successfully executed.")
 }
 
 // Down is executed when this migration is rolled back
-func Down_20201221012206(txn *gorm.DB) {
+func Down_20201221012206(db *gorm.DB) {
+	msg := "Characters migration 20201221012206 down!"
+	fmt.Println(msg)
 
+	err := db.Exec(`
+    ALTER TABLE characters DROP CONSTRAINT unique_nickname;
+	`).Error
+
+	if err != nil {
+		log.Println("Migration down error:", err)
+	}
+
+	fmt.Println(msg + "was successfully executed.")
 }
