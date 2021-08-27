747c4d7ba70cef13885670cfae9495801f0be5d8
Víctor Hugo Valle Castillo <victouk@gmail.com>
2021-08-27 20:40:45 -0300

    Updating test cases (first stage).

--- src/controllers/characters_test.go
+++ src/controllers/characters_test.go
@@ -1,17 +1,30 @@
 package controllers
 
 import (
+	"bytes"
+	"encoding/json"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
 	"github.com/gorilla/mux"
 	"github.com/stretchr/testify/assert"
+
+	"../databases"
+	"../models"
+	"../services"
+)
+
+var (
+	characterSt   databases.CharacterStorage = databases.NewSQLiteStorage()
+	characterSrv  services.CharacterService  = services.NewCharacterService(characterSt)
+	characterCtrl CharacterController        = NewCharacterController(characterSrv)
 )
 
 func Router() *mux.Router {
 	router := mux.NewRouter()
-	router.HandleFunc("/api/characters", GetCharacters).Methods("GET")
+	router.HandleFunc("/api/characters", characterCtrl.GetCharacters).Methods("GET")
+	router.HandleFunc("/api/characters", characterCtrl.CreateCharacter).Methods("POST")
 	return router
 }
 
@@ -20,5 +33,21 @@ func TestGetCharacters(t *testing.T) {
 	response := httptest.NewRecorder()
 
 	Router().ServeHTTP(response, request)
+	t.Logf("!---> RESPONSE: %v", response)
 	assert.Equal(t, 200, response.Code, "The expected status code must be 200.")
+}
+
+func TestCreateCharacter(t *testing.T) {
+	character := models.Character{
+		FirstName: "John",
+		LastName:  "Doe",
+		Nickname:  "johnny",
+	}
+	jsonCharacter, _ := json.Marshal(&character)
+
+	request, _ := http.NewRequest("POST", "/api/characters", bytes.NewBuffer(jsonCharacter))
+	response := httptest.NewRecorder()
+
+	Router().ServeHTTP(response, request)
+	assert.Equal(t, 201, response.Code, "The expected status code must be 201.")
 }
--- src/services/characters_test.go
+++ src/services/characters_test.go
@@ -1,32 +1,69 @@
 package services
 
 import (
-	"encoding/json"
+	"fmt"
+	"reflect"
 	"testing"
 
-	"../models"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+
+	"../mocks"
+	"../models"
 )
+
+type MockCharacterStorage struct {
+	mock.Mock
+}
+
+func (mock *MockCharacterStorage) Create(character *models.Character) (*models.CreatedRecordResponse, error) {
+	args := mock.Called()
+	response := args.Get(0)
+	return response.(*models.CreatedRecordResponse), args.Error(1)
+}
+
+func (mock *MockCharacterStorage) FindAll() ([]models.Character, error) {
+	args := mock.Called()
+	response := args.Get(0)
+	fmt.Printf("|---> CHARACTERS - TEST RESPONSE: %v", reflect.TypeOf(response.([]models.Character)))
+	return response.([]models.Character), args.Error(1)
+}
 
 func TestGetCharacters(t *testing.T) {
 	// Arrange
-	var expectedTotalType int16
+	mockRepo := new(MockCharacterStorage)
+
+	//var expectedTotalType int16
 	var expectedCharactersType []models.Character
-	var expectedLinkType models.Link
+	//var expectedLinkType models.Link
 
-	// Atc
-	charactersResponse, charsErr := GetCharacters()
+	characters := mocks.GenerateCharacters()
 
-	var response models.CharactersResponse
-	json.Unmarshal(charactersResponse, &response)
+	mockRepo.On("FindAll").Return(characters, nil)
 
-	// Asset
-	assert.IsType(t, expectedTotalType, response.Total, "The Total property has a wrong type.")
-	assert.IsType(t, expectedCharactersType, response.Characters, "The Characters property has a wrong type.")
-	assert.IsType(t, expectedLinkType, response.Link, "The Link property has a wrong type.")
+	testService := NewCharacterService(mockRepo)
 
-	assert.Equal(t, int16(3), response.Total, "Expected to get 3 characters but, %v were retrieved instead.", response.Total)
-	assert.NotEmpty(t, response.Characters, "The list of mocked characters should not be empty.")
+	response, _ := testService.FindAll()
 
-	assert.Nil(t, charsErr)
+	mockRepo.AssertExpectations(t)
+
+	assert.IsType(t, expectedCharactersType, response, "The Characters property has a wrong type.")
+
+	/*
+		// Atc
+		charactersResponse, charsErr := GetCharacters()
+
+		var response models.CharactersResponse
+		json.Unmarshal(charactersResponse, &response)
+
+		// Asset
+		assert.IsType(t, expectedTotalType, response.Total, "The Total property has a wrong type.")
+		assert.IsType(t, expectedCharactersType, response.Characters, "The Characters property has a wrong type.")
+		assert.IsType(t, expectedLinkType, response.Link, "The Link property has a wrong type.")
+
+		assert.Equal(t, int16(3), response.Total, "Expected to get 3 characters but, %v were retrieved instead.", response.Total)
+		assert.NotEmpty(t, response.Characters, "The list of mocked characters should not be empty.")
+
+		assert.Nil(t, charsErr)
+	*/
 }
